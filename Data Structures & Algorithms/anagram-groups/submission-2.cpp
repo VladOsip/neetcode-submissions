2bc8dec0c87df93f760195ff5af0f8c9fb0b86a5
VladOsip <89343585+VladOsip@users.noreply.github.com>
2026-06-15 00:01:41 +0300

    Add: anagram-groups - submission-2

---
+++ Data Structures & Algorithms/anagram-groups/submission-2.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    std::vector<std::vector<std::string>> groupAnagrams(std::vector<std::string>& strs) {
+        std::unordered_map<std::string, std::vector<std::string>> key_map;
+
+        for (const std::string& str : strs) {
+            std::string my_key(26, '0');
+            for (char c : str) {
+                my_key[c - 'a'] += 1;
+            }
+            key_map[my_key].push_back(str);
+        }
+
+        std::vector<std::vector<std::string>> values;
+        
+        for (const auto& value : key_map) {
+            values.push_back(value.second); 
+        }
+
+        return values;
+    }
+};
